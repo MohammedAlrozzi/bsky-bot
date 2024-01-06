5d083fb11aefc7d5937a754e5e88d14951eb3569
Mohammed ALRUZZI <irozzi@icloud.com>
2024-01-06 23:20:19 +0000

    test

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,4 +1,9 @@
+// export default async function getPostText() {
+//   // Generate the text for your post here. You can return a string or a promise that resolves to a string
+//   return "Palestine will be free";
+// }
 export default async function getPostText() {
-  // Generate the text for your post here. You can return a string or a promise that resolves to a string
-  return "Palestine will be free";
+  const options = ["From the river to the sea", "Palestine will be free", "Justice will be served", "Free Free Palestine"]; // Add your options here
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
 }
