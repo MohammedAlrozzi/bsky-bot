5dc6a8a505faa9a97fc3fe91b456ecaff5dd0ee1
Mohammed ALRUZZI <irozzi@icloud.com>
2024-01-07 09:57:00 +0000

    t14

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,18 +1,22 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
 
 export default async function getPostText() {
   const url = 'https://www.aljazeera.com/news/longform/2023/10/9/israel-hamas-war-in-maps-and-charts-live-tracker';
 
   const response = await axios.get(url);
-  const $ = cheerio.load(response.data);
+  const htmlContent = response.data;
 
-  // Replace the selector with the actual selector for the number you want to extract
-  const number = $('your_selector').text();
+  const startIndex = htmlContent.indexOf(' at ') + 4; // 4 is the length of ' at '
+  const endIndex = htmlContent.indexOf(' Palestinians', startIndex);
 
-  return number;
+  if (startIndex < 4 || endIndex === -1) {
+    throw new Error('Could not find the target string in the HTML content');
+  }
+
+  const extractedText = htmlContent.slice(startIndex, endIndex);
+
+  return extractedText;
 }
-
 // export default async function getPostText() {
 //   const options = ["From the river to the sea", "Palestine will be free", "Justice will be served", "Free Free Palestine", "what do we want? - Justice. When do we want it? - Now"]; // Add your options here
 //   const randomIndex = Math.floor(Math.random() * options.length);
