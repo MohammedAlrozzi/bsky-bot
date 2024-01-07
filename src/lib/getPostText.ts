454861a248e0e5106319c8bfc7944afeed064481
Mohammed ALRUZZI <irozzi@icloud.com>
2024-01-07 09:43:26 +0000

    test

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,5 +1,18 @@
 export default async function getPostText() {
-  const options = ["From the river to the sea", "Palestine will be free", "Justice will be served", "Free Free Palestine", "what do we want? - Justice. When do we want it? - Now"]; // Add your options here
-  const randomIndex = Math.floor(Math.random() * options.length);
-  return options[randomIndex];
+  const url = 'https://www.aljazeera.com/news/longform/2023/10/9/israel-hamas-war-in-maps-and-charts-live-tracker?text=<at>123</Palestinians>';
+  const startTag = '<at>';
+  const endTag = '</Palestinians>';
+  
+  const startIndex = url.indexOf(startTag) + startTag.length;
+  const endIndex = url.indexOf(endTag);
+  
+  const number = url.substring(startIndex, endIndex);
+  return number
+// console.log(number); // 123
 }
+
+// export default async function getPostText() {
+//   const options = ["From the river to the sea", "Palestine will be free", "Justice will be served", "Free Free Palestine", "what do we want? - Justice. When do we want it? - Now"]; // Add your options here
+//   const randomIndex = Math.floor(Math.random() * options.length);
+//   return options[randomIndex];
+// }
