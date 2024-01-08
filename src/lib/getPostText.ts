b9c6157161a604a99ab64d7bdff41e7a760d4c85
Mohammed ALRUZZI <irozzi@icloud.com>
2024-01-08 20:20:05 +0000

    test

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import cheerio from 'cheerio';
+
 
 export default async function getPostText() {
   const url = 'https://www.aljazeera.com/news/longform/2023/10/9/israel-hamas-war-in-maps-and-charts-live-tracker';
@@ -9,7 +11,10 @@ export default async function getPostText() {
     }
   });
   const htmlContent = response.data;
-
+  const $ = cheerio.load(htmlContent);
+  const updateTimeElement = $('.date-updated__date');
+  const updateTimeText = updateTimeElement.text();
+  
   const startIndex = htmlContent.indexOf(' at ') + 4; // 4 is the length of ' at '
   const endIndex = htmlContent.indexOf(' Palestinians', startIndex);
 
@@ -20,22 +25,13 @@ export default async function getPostText() {
   const extractedText = htmlContent.slice(startIndex, endIndex);
 
 
-  const startIndex2 = htmlContent.indexOf('Here are the latest casualty figures as of ') + 43; // 4 is the length of ' at '
-  const endIndex2 = htmlContent.indexOf(' in Gaza (', startIndex2);
-
-  if (startIndex2 < 43 || endIndex2 === -1) {
-    throw new Error('Could not find the target string in the HTML content');
-  }
-
-  const extractedText2 = htmlContent.slice(startIndex2, endIndex2);
-
 
 
   const date = new Date();
   const options: any = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'Asia/Jerusalem' };
   const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
 
-  const finalText = `-- ${formattedDate} (Gaza time):\nIsrael killed more than ${extractedText} Palestinians, in the last 3 months alone.\n\nThis data was last updated on ${extractedText2} Gaza time.`;
+  const finalText = `-- ${formattedDate} (Gaza time):\nIsrael killed more than ${extractedText} Palestinians, in the last 3 months alone.\n\nThis data was last updated ${updateTimeText} ago.`;
 
   return finalText;
 }
