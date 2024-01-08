5d8f7a6a61a1a1834154c0c985c262dfe2b32436
Mohammed ALRUZZI <irozzi@icloud.com>
2024-01-08 23:16:40 +0000

    t40

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -1,105 +1,61 @@
-// import { bskyAccount, bskyService } from "./config.js";
-// import type {
-//   AtpAgentLoginOpts,
-//   AtpAgentOpts,
-//   AppBskyFeedPost,
-// } from "@atproto/api";
-// import atproto from "@atproto/api";
-// const { BskyAgent, RichText } = atproto;
-
-// type BotOptions = {
-//   service: string | URL;
-//   dryRun: boolean;
-// };
-
-// export default class Bot {
-//   #agent;
-
-//   static defaultOptions: BotOptions = {
-//     service: bskyService,
-//     dryRun: false,
-//   } as const;
-
-//   constructor(service: AtpAgentOpts["service"]) {
-//     this.#agent = new BskyAgent({ service });
-//   }
-
-//   login(loginOpts: AtpAgentLoginOpts) {
-//     return this.#agent.login(loginOpts);
-//   }
-
-//   async post(
-//     text:
-//       | string
-//       | (Partial<AppBskyFeedPost.Record> &
-//           Omit<AppBskyFeedPost.Record, "createdAt">)
-//   ) {
-//     if (typeof text === "string") {
-//       const richText = new RichText({ text });
-//       await richText.detectFacets(this.#agent);
-//       const record = {
-//         text: richText.text,
-//         facets: richText.facets,
-//       };
-//       return this.#agent.post(record);
-//     } else {
-//       return this.#agent.post(text);
-//     }
-//   }
-
-//   static async run(
-//     getPostText: () => Promise<string>,
-//     botOptions?: Partial<BotOptions>
-//   ) {
-//     const { service, dryRun } = botOptions
-//       ? Object.assign({}, this.defaultOptions, botOptions)
-//       : this.defaultOptions;
-//     const bot = new Bot(service);
-//     await bot.login(bskyAccount);
-//     const text = await getPostText();
-//     if (!dryRun) {
-//       await bot.post(text);
-//     }
-//     return text;
-//   }
-// }
-import Twit from 'twit';
-import { twitterConfig } from './config.js';
+import { bskyAccount, bskyService } from "./config.js";
+import type {
+  AtpAgentLoginOpts,
+  AtpAgentOpts,
+  AppBskyFeedPost,
+} from "@atproto/api";
+import atproto from "@atproto/api";
+const { BskyAgent, RichText } = atproto;
 
 type BotOptions = {
+  service: string | URL;
   dryRun: boolean;
 };
 
 export default class Bot {
-  #twit;
+  #agent;
 
   static defaultOptions: BotOptions = {
+    service: bskyService,
     dryRun: false,
   } as const;
 
-  constructor() {
-    this.#twit = new Twit(twitterConfig);
+  constructor(service: AtpAgentOpts["service"]) {
+    this.#agent = new BskyAgent({ service });
   }
 
-  async post(text: string) {
-    return new Promise((resolve, reject) => {
-      if (!Bot.defaultOptions.dryRun) {
-        this.#twit.post('statuses/update', { status: text }, (err, data, response) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(data);
-          }
-        });
-      }
-    });
+  login(loginOpts: AtpAgentLoginOpts) {
+    return this.#agent.login(loginOpts);
   }
-  
-  static async run(getPostText: () => Promise<string>, botOptions?: Partial<BotOptions>) {
-    const { dryRun } = botOptions
-      ? Object.assign({}, Bot.defaultOptions, botOptions)
-      : Bot.defaultOptions;
-    const bot = new Bot();
+
+  async post(
+    text:
+      | string
+      | (Partial<AppBskyFeedPost.Record> &
+          Omit<AppBskyFeedPost.Record, "createdAt">)
+  ) {
+    if (typeof text === "string") {
+      const richText = new RichText({ text });
+      await richText.detectFacets(this.#agent);
+      const record = {
+        text: richText.text,
+        facets: richText.facets,
+      };
+      return this.#agent.post(record);
+    } else {
+      return this.#agent.post(text);
+    }
+  }
+
+  static async run(
+    getPostText: () => Promise<string>,
+    botOptions?: Partial<BotOptions>
+  ) {
+    const { service, dryRun } = botOptions
+      ? Object.assign({}, this.defaultOptions, botOptions)
+      : this.defaultOptions;
+    const bot = new Bot(service);
+    await bot.login(bskyAccount);
     const text = await getPostText();
     if (!dryRun) {
       await bot.post(text);
@@ -107,3 +63,47 @@ export default class Bot {
     return text;
   }
 }
+// import Twit from 'twit';
+// import { twitterConfig } from './config.js';
+
+// type BotOptions = {
+//   dryRun: boolean;
+// };
+
+// export default class Bot {
+//   #twit;
+
+//   static defaultOptions: BotOptions = {
+//     dryRun: false,
+//   } as const;
+
+//   constructor() {
+//     this.#twit = new Twit(twitterConfig);
+//   }
+
+//   async post(text: string) {
+//     return new Promise((resolve, reject) => {
+//       if (!Bot.defaultOptions.dryRun) {
+//         this.#twit.post('statuses/update', { status: text }, (err, data, response) => {
+//           if (err) {
+//             reject(err);
+//           } else {
+//             resolve(data);
+//           }
+//         });
+//       }
+//     });
+//   }
+  
+//   static async run(getPostText: () => Promise<string>, botOptions?: Partial<BotOptions>) {
+//     const { dryRun } = botOptions
+//       ? Object.assign({}, Bot.defaultOptions, botOptions)
+//       : Bot.defaultOptions;
+//     const bot = new Bot();
+//     const text = await getPostText();
+//     if (!dryRun) {
+//       await bot.post(text);
+//     }
+//     return text;
+//   }
+// }
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,24 +1,24 @@
-// import { env } from "node:process";
-// import { z } from "zod";
-// import type { AtpAgentLoginOpts } from "@atproto/api";
+import { env } from "node:process";
+import { z } from "zod";
+import type { AtpAgentLoginOpts } from "@atproto/api";
 
-// const envSchema = z.object({
-//   BSKY_HANDLE: z.string().nonempty(),
-//   BSKY_PASSWORD: z.string().nonempty(),
-//   BSKY_SERVICE: z.string().nonempty().default("https://bsky.social"),
-// });
+const envSchema = z.object({
+  BSKY_HANDLE: z.string().nonempty(),
+  BSKY_PASSWORD: z.string().nonempty(),
+  BSKY_SERVICE: z.string().nonempty().default("https://bsky.social"),
+});
 
-// const parsed = envSchema.parse(env);
+const parsed = envSchema.parse(env);
 
-// export const bskyAccount: AtpAgentLoginOpts = {
-//   identifier: parsed.BSKY_HANDLE,
-//   password: parsed.BSKY_PASSWORD,
-// };
-
-// export const bskyService = parsed.BSKY_SERVICE;
-export const twitterConfig = {
-  consumer_key: 'zmEna2xd5pwakykKhJvMGWujc',
-  consumer_secret: 'UtByzhHArs0bR0j5QqxZ3bA3JSqmzG2SyT43LRtXLL2eeAyPy4',
-  access_token: '243448555-PfPPClWzgU2raJdu5DJgzMAjTgpDGzyO0ZLPropk',
-  access_token_secret: 'bQNa0osgnLPhOEtPPn60XGT8dgab2u5S63SL8SfkmhixO'
+export const bskyAccount: AtpAgentLoginOpts = {
+  identifier: parsed.BSKY_HANDLE,
+  password: parsed.BSKY_PASSWORD,
 };
+
+export const bskyService = parsed.BSKY_SERVICE;
+// export const twitterConfig = {
+//   consumer_key: '',
+//   consumer_secret: '',
+//   access_token: '',
+//   access_token_secret: ''
+// };
