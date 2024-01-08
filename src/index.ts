c4cd5d6f1ed844025dd826e1c22d7693732506d1
Mohammed ALRUZZI <irozzi@icloud.com>
2024-01-08 22:58:46 +0000

    y

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+// import Bot from "./lib/bot.js";
+// import getPostText from "./lib/getPostText.js";
+
+// const text = await Bot.run(getPostText);
+
+// console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 
-const text = await Bot.run(getPostText);
+async function main() {
+  const text = await Bot.run(getPostText);
+  console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
+}
 
-console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
+main().catch(error => {
+  console.error(error);
+});
