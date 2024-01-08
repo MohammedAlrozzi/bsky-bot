7fa24b880d2146918abcbd32b779a4392b2bc201
Mohammed ALRUZZI <irozzi@icloud.com>
2024-01-08 14:05:03 +0000

    t7

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,93 +1,26 @@
-// import axios from 'axios';
-
-// export default async function getPostText() {
-//   const url = 'https://www.aljazeera.com/news/longform/2023/10/9/israel-hamas-war-in-maps-and-charts-live-tracker';
-
-//   const response = await axios.get(url);
-//   const htmlContent = response.data;
-
-//   const startIndex = htmlContent.indexOf(' at ') + 4; // 4 is the length of ' at '
-//   const endIndex = htmlContent.indexOf(' Palestinians', startIndex);
-
-//   if (startIndex < 4 || endIndex === -1) {
-//     throw new Error('Could not find the target string in the HTML content');
-//   }
-
-//   const extractedText = htmlContent.slice(startIndex, endIndex);
-
-//   const finalText = `Israel killed more than ${extractedText} Palestinians, in the last 3 months alone.`;
-
-
-//   return finalText;
-// }
-// export default async function getPostText() {
-//   const options = ["From the river to the sea", "Palestine will be free", "Justice will be served", "Free Free Palestine", "what do we want? - Justice. When do we want it? - Now"]; // Add your options here
-//   const randomIndex = Math.floor(Math.random() * options.length);
-//   return options[randomIndex];
-// }
-// import axios from 'axios';
-
-// export default async function getPostText() {
-//   const url = 'https://www.aljazeera.com/news/longform/2023/10/9/israel-hamas-war-in-maps-and-charts-live-tracker';
-
-//   const response = await axios.get(url);
-//   const htmlContent = response.data;
-
-//   const startIndex = htmlContent.indexOf(' at ') + 4; // 4 is the length of ' at '
-//   const endIndex = htmlContent.indexOf(' Palestinians', startIndex);
-
-//   if (startIndex < 4 || endIndex === -1) {
-//     throw new Error('Could not find the target string in the HTML content');
-//   }
-
-//   const extractedText = htmlContent.slice(startIndex, endIndex);
-
-
-//   const date = new Date();
-//   const options: any = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'Asia/Jerusalem' };
-//   const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
-
-//   const finalText = `-- ${formattedDate} (Gaza time):\nIsrael killed more than ${extractedText} Palestinians, in the last 3 months alone.`;
-
-//   return finalText;
-// }
 import axios from 'axios';
+
 export default async function getPostText() {
   const url = 'https://www.aljazeera.com/news/longform/2023/10/9/israel-hamas-war-in-maps-and-charts-live-tracker';
 
   const response = await axios.get(url);
   const htmlContent = response.data;
 
-  // First extraction
-  const startIndex1 = htmlContent.indexOf(' at ') + 4; // 4 is the length of ' at '
-  const endIndex1 = htmlContent.indexOf(' Palestinians', startIndex1);
+  const startIndex = htmlContent.indexOf(' at ') + 4; // 4 is the length of ' at '
+  const endIndex = htmlContent.indexOf(' Palestinians', startIndex);
 
-  if (startIndex1 < 4 || endIndex1 === -1) {
+  if (startIndex < 4 || endIndex === -1) {
     throw new Error('Could not find the target string in the HTML content');
   }
 
-  const extractedText1 = htmlContent.slice(startIndex1, endIndex1);
+  const extractedText = htmlContent.slice(startIndex, endIndex);
 
-  // Second extraction
-  const startIndicator2 = "Updated: ";
-  const endIndicator2 = " (GMT";
-  let startIndex2 = htmlContent.indexOf(startIndicator2);
-  let endIndex2 = htmlContent.indexOf(endIndicator2, startIndex2 + startIndicator2.length);
-  
-  if (startIndex2 === -1 || endIndex2 === -1) {
-    console.log('Could not find the target string in the HTML content');
-    extractedText2 = "random text";
-  } else {
-    startIndex2 += startIndicator2.length;
-    extractedText2 = htmlContent.slice(startIndex2, endIndex2);
-  }
 
   const date = new Date();
   const options: any = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'Asia/Jerusalem' };
   const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
 
-  const finalText = `-- ${formattedDate} (Gaza time):\nIsrael killed more than ${extractedText1} Palestinians, in the last 3 months alone. ${extractedText2}`;
+  const finalText = `-- ${formattedDate} (Gaza time):\nIsrael killed more than ${extractedText} Palestinians, in the last 3 months alone.`;
 
   return finalText;
-  // Now you have both extractedText1 and extractedText2
 }
